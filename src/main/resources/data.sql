2719f65e17428c8985b79d11299c03edaf113d7d
imranmr <imran.mohdraji@gmail.com>
2021-11-24 10:09:22 -0800

    Added docker file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,22 @@
+insert into user (userid, adminrights, email, firstname,lastname, mobile, password) values (1,1,"imran.raji@gmail.com","Imran","Raji","2223334444","admin");
+
+insert into product (productid, price, productname, productquantity, userid) value (1, 2, "Apples", 20, 1);
+insert into product (productid, price, productname, productquantity, userid) value (2, 2, "Mango", 20, 1);
+insert into product (productid, price, productname, productquantity, userid) value (3, 2, "Grapes", 20, 1);
+insert into product (productid, price, productname, productquantity, userid) value (4, 2, "Watermelon", 20, 1);
+
+insert into product (productid, price, productname, productquantity, userid) value (5, 5, "Chicken", 20, 1);
+insert into product (productid, price, productname, productquantity, userid) value (6, 5, "Lamb", 20, 1);
+insert into product (productid, price, productname, productquantity, userid) value (7, 5, "Beef", 20, 1);
+insert into product (productid, price, productname, productquantity, userid) value (8, 5, "Fish", 20, 1);
+
+insert into productdetail (productdetailid, type, productid) value (1, "Fruits",1);
+insert into productdetail (productdetailid, type, productid) value (2, "Fruits",2);
+insert into productdetail (productdetailid, type, productid) value (3, "Fruits",3);
+insert into productdetail (productdetailid, type, productid) value (4, "Fruits",4);
+
+insert into productdetail (productdetailid, type, productid) value (5, "Meats",5);
+insert into productdetail (productdetailid, type, productid) value (6, "Meats",6);
+insert into productdetail (productdetailid, type, productid) value (7, "Meats",7);
+insert into productdetail (productdetailid, type, productid) value (8, "Meats",8);
+
